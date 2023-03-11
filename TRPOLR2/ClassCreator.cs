4e9afb7ce1642f4683095fb815ea5078cbc6bce3
DaniilShilov765 <shilovdaniil765@mail.ru>
2023-03-11 22:46:55 +0300

    Введены новые заглушки и класс Занятие

---
+++ TRPOLR2/ClassCreator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryTRPOLR2;
+
+namespace TRPOLR2
+{
+    static class ClassCreator
+    {
+        public static Занятие Занятие()
+        {
+            Console.WriteLine($"Введите текущую дату: ");
+            string Дата_проведения = Console.ReadLine();
+            return new Занятие(Дата_проведения, Дисциплина(), Сотрудник(), Аудитория(), Группа(), Пара(), Вид_занятия());
+        }
+        public static Аудитория Аудитория()
+        {
+            return new Аудитория();
+        }
+        public static Вид_занятия Вид_занятия()
+        {
+            return new Вид_занятия();
+        }
+        public static Группа Группа()
+        {
+            return new Группа();
+        }
+        public static Дисциплина Дисциплина()
+        {
+            return new Дисциплина();
+        }
+        public static Пара Пара()
+        {
+            return new Пара();
+        }
+        public static Сотрудник Сотрудник()
+        {
+            return new Сотрудник();
+        }
+    }
+}
