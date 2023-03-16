f9ce6ff69ab2bd752c1347e6d767aff65f8d57fe
Owl_26 <varechka_koroleva@list.ru>
2023-03-16 15:02:55 +0300

    Реализован класс Пара и заглушки для него

--- TRPOLR2/Program.cs
+++ TRPOLR2/Program.cs
@@ -18,6 +18,7 @@ namespace TRPOLR2
                 {
                     case "Занятие": UI.Print(ClassCreator.Занятие()); break;
                     case "Аудитория": UI.Print(ClassCreator.Аудитория()); break;
+                    case "Пара": UI.Print(ClassCreator.Пара()); break;
                     default: Console.WriteLine("Этот класс не реализован"); break;
                 }
             }
