0e792b7bb76c29e78593acecebd44a0ca81655d3
local2deb <kuznecovmisa428@gmail.com>
2023-04-01 00:30:38 +0300

    Реализован класс Оборудование

--- ClassLibraryTRPOLR2/Оборудование.cs
+++ ClassLibraryTRPOLR2/Оборудование.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace ClassLibraryTRPOLR2
 {
+    /// <summary>
+    /// Task 15 url: https://docs.google.com/document/d/1ynMOoROmPiCWqPt78fnwQoIJKsoAIgdxhzp1VSjatsE/edit
+    /// </summary>
     public class Оборудование
     {
+        public string Название { get; }
+        public decimal Стоимость { get; }
+        public DateTime Дата_постановки_на_баланс { get; }
+
+        public Оборудование(string название, decimal стоимость, DateTime дата_постановки_на_баланс)
+        {
+            Название = название;
+            Стоимость = стоимость;
+            Дата_постановки_на_баланс = дата_постановки_на_баланс;
+        }
     }
 }
