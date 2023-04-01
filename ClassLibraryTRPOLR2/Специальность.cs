34c9ba90964befe865ccc85f671a633e95a87e16
Vyacheslav Nikonov <slavkanik2004@gmail.com>
2023-04-01 07:24:54 +0000

    Update file Специальность.cs

--- ClassLibraryTRPOLR2/Специальность.cs
+++ ClassLibraryTRPOLR2/Специальность.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassLibraryTRPOLR2
 {
-    public class Специальность
+    public class Специальность //https://docs.google.com/document/d/1pDw7bbBJDmeqT8VVwp68BSOF_4yd9cNb9xpH_q2KW0E/edit?usp=share_link
     {
         private string название { get; }
         private string сокращение { get; }
