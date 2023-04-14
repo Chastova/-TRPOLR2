47a4d78668c3db6b9757449d5f25141c45b8b94b
yurin_vanechka <yurin_vanechka@mail.ru>
2023-04-14 21:09:22 +0300

    Реализован класс подразделение

--- TRPOLR2/ClassCreator.cs
+++ TRPOLR2/ClassCreator.cs
@@ -245,14 +245,13 @@ namespace TRPOLR2
             string Отчество = Console.ReadLine();
             return new Сотрудник(Фамилия, Имя, Отчество, Должность());
         }
-		public static Подразделение Подразделение()
-		{
-			//Console.WriteLine($"Введите название: ");
-			//string Название = Console.ReadLine();
-			//Console.WriteLine();
-
-			return new Подразделение( /*Название, Руководитель(), Организация() */);
-		}
+        public static Подразделение Подразделение()
+        {
+            Console.WriteLine($"Введите название: ");
+            string Название = Console.ReadLine();
+            Console.WriteLine();
+            return new Подразделение( Название, Руководитель(), Организация() );
+        }
 		public static Должность Должность()
         {
             return new Должность();
